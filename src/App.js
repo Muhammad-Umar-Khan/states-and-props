6ddf30368b56acd4cc4b60c2b0aa23973044455f
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2022-09-26 18:41:32 +0500

    Image tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Button from "./components/button";
+import Image from "./components/image";
 
 function App() {
   return (
     <div>
       <Button age={23} />
+      <Image />
     </div>
   );
 }
