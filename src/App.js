4e155d4efb12b578d625fb12a164fc7d4f602a77
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2022-09-27 14:24:29 +0500

    todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/button";
 import Image from "./components/image";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { text: "learn hooks" },
+    { text: "learn useEffect()" },
+  ]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, { text: newTask }]);
+    setNewTask("");
+  };
+  useEffect(() => {
+    console.log("rendred");
+  }, []);
   return (
     <div>
-      <Button age={23} />
-      <Image />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setNewTask(e.target.value)}
+          value={newTask}
+        ></input>
+        <button>Add</button>
+      </form>
+      {todos.map((todo, index) => {
+        return (
+          <div key={index}>
+            <li>{todo.text}</li>
+          </div>
+        );
+      })}
     </div>
   );
 }
